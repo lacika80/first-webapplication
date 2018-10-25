6bd22c05f63bbbe8128ec46efd1f5580de77f0cf
Gyurkovics László <gyurkovics95@gmail.com>
2018-10-25 15:59:11 +0200

    1 új lap létrehozása, lista kiterjesztés definiálása, a listán foreach-elés

--- WebApp/WebApp/Controllers/HomeController.cs
+++ WebApp/WebApp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
@@ -25,6 +26,17 @@ namespace WebApp.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult TodoList()
+        {
+            List<Item> lista = new List<Item>();
+            lista.Add(new Item() { Name = "só", Quantity = 2, Unit = "kg", Done = true });
+            lista.Add(new Item() { Name = "cukor", Quantity = 1, Unit = "kg", Done = false });
+            lista.Add(new Item() { Name = "liszt", Quantity = 500, Unit = "gramm", Done = false });
+            lista.Add(new Item() { Name = "kömény", Quantity = 1, Unit = "tasak", Done = false });
+            ViewBag.Lista = lista;
+            return View(lista);
         }
     }
 }
---
+++ WebApp/WebApp/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class Item
+    {
+        public string Name { get; internal set; }
+        public int Quantity { get; internal set; }
+        public string Unit { get; internal set; }
+        public bool Done { get; internal set; }
+    }
+}
